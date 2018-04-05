22eb045a9e9f1349d50febaf14abfd39f91b7092
Menna Anees <mennaanees193@gmail.com>
2018-04-05 22:55:22 +0200

    add routing

--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 export default class Login extends Component {
 
   render() {
     return (
-
-      <div  className='login-form'>
-      <p>
-      hii
-      </p>
-    </div>
+<Router>
+      <h2 className="mb-5">Form login</h2>
+  <form>
+      <p className="h5 text-center mb-4">Sign in</p>
+      <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+      <Input label="Type your password" icon="lock" group type="password" validate/>
+      <div className="text-center">
+          <Button>Login</Button>
+      </div>
+  </form>
+  </Router>
 
     );
   }
