3b859b435bdb6ea69049e82f553973561447f2a5
Menna Anees <mennaanees193@gmail.com>
2018-04-05 23:40:37 +0200

    login component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import Login from './components/login';
+import Orders from './components/orders';
+import Friend from './components/friend';
+import Register from './components/register';
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div classNAme="App">
+      <div className="App">
       <Router>
-      <div>
+          <div>
+              <Route path="/friends" Component={Friend} />
+          </div>
+          <div>
+              <Route exact path="/Orders" Component={Orders} />
+          </div>
+          <div>
               <Route exact path="/Login" Component={Login} />
+          </div>
+          <div>
+              <Route exact path="/Register" Component={Register} />
           </div>
       </Router>
       </div>
--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink } from 'mdbreact';
+import { Button, Input } from 'mdbreact';
 
 
 export default class Login extends Component {
 
   render() {
     return (
+<div>
+<h2 className="mb-5">Form login</h2>
+<form>
+    <p className="h5 text-center mb-4">Sign in</p>
+    <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+    <Input label="Type your password" icon="lock" group type="password" validate/>
+    <div className="text-center">
+        <Button>Login</Button>
 
+<div>
+        <a type="button" class="btn-floating btn-lg btn-fb" ><i class="fa fa-facebook"></i></a>
+        <a type="button" class="btn-floating btn-lg btn-gplus" ><i class="fa fa-google-plus"></i></a>
+        </div>
+    </div>
+</form>
+</div>
 
     );
   }
