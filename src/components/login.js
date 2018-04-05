16334d3261917fd0141f4b3760c8f9235ddf6b7c
Menna Anees <mennaanees193@gmail.com>
2018-04-06 00:06:52 +0200

    register component design

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Button, Input } from 'mdbreact';
+import { Button, Input, label } from 'mdbreact';
 
 
 export default class Login extends Component {
@@ -9,20 +9,51 @@ export default class Login extends Component {
   render() {
     return (
 <div>
-<h2 className="mb-5">Form login</h2>
-<form>
-    <p className="h5 text-center mb-4">Sign in</p>
-    <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
-    <Input label="Type your password" icon="lock" group type="password" validate/>
-    <div className="text-center">
-        <Button>Login</Button>
 
-<div>
-        <a type="button" class="btn-floating btn-lg btn-fb" ><i class="fa fa-facebook"></i></a>
-        <a type="button" class="btn-floating btn-lg btn-gplus" ><i class="fa fa-google-plus"></i></a>
+
+<section class="form-elegant">
+
+    <div class="card">
+
+        <div class="card-body mx-4">
+
+            <div class="text-center">
+                <h3 class="dark-grey-text mb-5"><strong>Sign in</strong></h3>
+            </div>
+
+            <div class="md-form">
+                <input type="text" id="Form-email1" class="form-control"/>
+                <label for="Form-email1">Your email</label>
+            </div>
+
+            <div class="md-form pb-3">
+                <input type="password" id="Form-pass1" class="form-control"/>
+                <label for="Form-pass1">Your password</label>
+                <p class="font-small blue-text d-flex justify-content-end">Forgot <a href="#" class="blue-text ml-1"> Password?</a></p>
+            </div>
+
+            <div class="text-center mb-3">
+                <button type="button" class="btn blue-gradient btn-block btn-rounded z-depth-1a">Sign in</button>
+            </div>
+            <p class="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2"> or Sign in with:</p>
+
+                <div class="row my-3 d-flex justify-content-center">
+                    <button type="button" class="btn btn-white btn-rounded mr-md-3 z-depth-1a"><i class="fa fa-facebook blue-text text-center"></i></button>
+
+                    <button type="button" class="btn btn-white btn-rounded z-depth-1a"><i class="fa fa-google-plus blue-text"></i></button>
+                </div>
+
         </div>
+
+        <div class="modal-footer mx-5 pt-3 mb-1">
+            <p class="font-small grey-text d-flex justify-content-end">Not a member? <a href="#" class="blue-text ml-1"> Sign Up</a></p>
+        </div>
+
     </div>
-</form>
+
+</section>
+
+
 </div>
 
     );
