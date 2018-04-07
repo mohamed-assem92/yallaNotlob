cfbb34f78819d482216dcf8765bbfcd460740708
Menna Anees <mennaanees193@gmail.com>
2018-04-07 15:20:27 +0200

    make home page dynamic

--- src/components/home.js
+++ src/components/home.js
@@ -3,181 +3,83 @@ import { Link } from "react-router-dom";
 
 
 export default class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      friendsArr :[],
+      ordersArr : []
+    }
 
+  }
+componentWillMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+      let friendsArr = json
+      this.setState({ friendsArr:friendsArr })
+      // console.log(currentName);
+    });
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+
+        let ordersArr = json
+        this.setState({ ordersArr:ordersArr })
+        console.log(ordersArr);
+      });
+}
   render() {
+    const names=[1,2,3];
+
+
     return (
 
-      <div class="row">
+<div class="row">
 
-          <div class="col">
+      <div class="col">
           <h3>Latest Orders</h3>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
+          {this.state.ordersArr.map(function(order, index){
+                   return <li key={ index } class="list-group-item">{order.name}</li>;
+                 })}
           </ul>
-          </div>
-
-          <div class="col">
-
-      </div>
-          <div class="col">
-
-
-<section class="pt-5 pb-3">
-
-  <div class="row">
-
-      <div class="col-lg-6">
-
-          <div class="mdb-feed">
-
-
-              <div class="news">
-
-
-                  <div class="label">
-                      <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-1-mini.jpg" class="rounded-circle z-depth-1-half"/>
-                  </div>
-
-                  <div class="excerpt">
-
-
-                      <div class="brief">
-                          <a class="name" text-decoration="underline" >menna</a> created an order from
-                          <a class="name">tom and basal</a>
-                      </div>
-
-
-
-
-                  </div>
-
-              </div>
-
-              <div class="news">
-
-                  <div class="label">
-                      <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(17)-mini.jpg" class="rounded-circle z-depth-1-half"/>
-                  </div>
-
-
-                  <div class="excerpt">
-
-
-                      <div class="brief">
-                          <a class="name">Anna Smith</a> added
-                          <a> 2 new illustrations</a>
-                          <div class="date">4 hours ago</div>
-                      </div>
-
-                      <div class="feed-footer">
-                          <a class="like">
-                              <i class="fa fa-heart"></i>
-                              <span>18 likes</span>
-                          </a>
-                      </div>
-
-                  </div>
-
-              </div>
-
-              <div class="news">
-
-
-                  <div class="label">
-                      <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9)-mini.jpg" class="rounded-circle z-depth-1-half"/>
-                  </div>
-
-
-                  <div class="excerpt">
-
-
-                      <div class="brief">
-                          <a class="name">Danny Moore</a> added you as a friend
-                          <div class="date">7 hours ago</div>
-                      </div>
-
-
-                      <div class="feed-footer">
-                          <a class="like">
-                              <i class="fa fa-heart"></i>
-                              <span>11 likes</span>
-                          </a>
-                      </div>
-
-                  </div>
-
-              </div>
-
-              <div class="news">
-
-                  <div class="label">
-                      <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(18)-mini.jpg" class="rounded-circle z-depth-1-half"/>
-                  </div>
-
-                  <div class="excerpt">
-
-                      <div class="brief">
-                          <a class="name">Lili Rose</a> posted on his page
-                          <div class="date">2 days ago</div>
-                      </div>
-
-                      <div class="added-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero inventore, iste quas libero eius?
-                          Vitae sint neque animi alias sunt dolor, accusantium ducimus, non placeat voluptate.</div>
-
-
-                      <div class="feed-footer">
-                          <a class="like">
-                              <i class="fa fa-heart"></i>
-                              <span>7 likes</span>
-                          </a>
-                      </div>
-
-                  </div>
-
-              </div>
-
-              <div class="news">
-
-
-                  <div class="label">
-                      <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20)-mini.jpg" class="rounded-circle z-depth-1-half"/>
-                  </div>
-
-                  <div class="excerpt">
-
-                      <div class="brief">
-                          <a class="name">Kate Harrison</a> added
-                          <a> 2 new photos</a> of you
-                          <div class="date">3 days ago</div>
-                      </div>
-
-
-
-                      <div class="feed-footer">
-                          <a class="like">
-                              <i class="fa fa-heart"></i>
-                              <span>53 likes</span>
-                          </a>
-                      </div>
-
-                  </div>
-
-              </div>
-
-          </div>
-
       </div>
 
-  </div>
-
-</section>
-
-          </div>
+      <div class="col">
       </div>
+
+     <div class="col">
+
+       <section class="pt-5 pb-3">
+           <div class="row">
+
+           {this.state.friendsArr.map(function(friend, index){
+                    return  <div class="col-lg-6">
+                       <div class="mdb-feed">
+                          <div class="news">
+
+                             <div class="label">
+                               <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-1-mini.jpg" class="rounded-circle z-depth-1-half"/>
+                              </div>
+
+                            <div class="excerpt">
+                              <div class="brief">
+                                  <a class="name" text-decoration="underline" >{friend.name}</a>
+                                  <p class="name">created an order from {friend.email}</p>
+                              </div>
+                           </div>
+
+                         </div>
+                      </div>
+                     </div>
+                  })}
+
+            </div>
+            </section>
+
+            </div>
+</div>
     );
   }
 }
