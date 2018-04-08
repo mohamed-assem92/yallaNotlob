57f484ec9c219dfcb472df9608a66298493ca20c
Menna Anees <mennaanees193@gmail.com>
2018-04-08 23:15:11 +0200

    redirect from register to login

--- src/components/register.js
+++ src/components/register.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import FileBase64 from 'react-file-base64';
-
+import ReactDOM from 'react-dom';
+import Login from './login';
 
 export default class Register extends Component {
   constructor(props){
@@ -51,15 +52,19 @@ export default class Register extends Component {
          email: this.state.email,
          psw: this.state.psw,
          conPsw: this.state.conPsw,
-         files: this.state.files
+         files: this.state.files.base64
        };
        console.log({user})
-       fetch('https://jsonplaceholder.typicode.com/users', {
+     fetch('http://localhost:3001/users', {
      method: 'POST',
      body: JSON.stringify({user})
     }).then(res => res.json())
     .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
+    .then(response =>{ if(response.status == true){
+      ReactDOM.render(<Login />, document.getElementById('root'));
+    }
+  }
+  );
 
 
      }
@@ -135,7 +140,7 @@ export default class Register extends Component {
        onDone={ this.getFiles.bind(this) } />
     <div class="text-center mt-4">
 
-        <button class="btn btn-blue" type="submit">Register</button>
+        <button class="btn btn-blue" onChange type="submit">Register</button>
     </div>
 </form>
 
